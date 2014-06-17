c089f223933b15d37189d580a056a2232bcb8a24
Adam <asv-github@mit.edu>
2014-06-16 21:13:06 -0400

    Syntax error.

--- sources.py
+++ sources.py
@@ -6,8 +6,8 @@ def getBTCmessage():
 	# Gets the current price of Bitcoin from BitcoinAverage.com
 	# Don't call this more than once a minute -- it will make BitcoinAverage.com sad if we overload their servers.
 	btc_api_url = "https://api.bitcoinaverage.com/ticker/USD/24h_avg"
-	try
+	try:
 		data = urllib2.urlopen(btc_api_url).read()
 		return "1 BTC = $"+data
-	except
+	except:
 		return "Bitcoin price unavailable."
